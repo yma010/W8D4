2bb8003ada49e493f6b880d7f497782f0ce0d651
Marvin Ma <yma010@ucr.edu>
2019-08-22 15:20:56 -0700

    Phase 2 Complete Co-authored-by: Patrick M <patrickmondala@gmail.com> Patrick-mondala

--- ActiveRecordLite/lib/02_searchable.rb
+++ ActiveRecordLite/lib/02_searchable.rb
@@ -4,9 +4,23 @@ require_relative '01_sql_object'
 module Searchable
   def where(params)
     # ...
+    col_name = params.keys.map(&:to_s).join(" = ? AND ")
+    col_name += " = ?"
+
+    data = DBConnection.execute(<<-SQL, *params.values)
+      SELECT
+        *
+      FROM
+        #{self.table_name}
+      WHERE
+        #{col_name}
+    SQL
+
+    parse_all(data)
   end
 end
 
 class SQLObject
   # Mixin Searchable here...
+  extend Searchable
 end
